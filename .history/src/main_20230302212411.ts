a0229deaa09da299a683336a00507d02481074ec
liuche <lzc706716852@gmail.com>
2023-03-19 20:02:00 +0800

    没改啥，就是换电脑了直接从钱买你拷过来了有个这个看着不爽

--- .history/src/main_20230302212411.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Antd from 'ant-design-vue';
-import App from './App';
-import 'ant-design-vue/dist/antd.css';
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
